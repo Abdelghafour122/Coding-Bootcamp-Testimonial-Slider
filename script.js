b6c9d421c2091d2839a5d3a0635e71091dfa5abb
Abdelghafour <ghafourragg@gmail.com>
2022-03-15 19:30:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-const johnImg =
-  "http://127.0.0.1:5500/coding-bootcamp-testimonial-slider/images/image-john.jpg";
+const johnImg = "/images/image-john.jpg";
 const johnQuote =
   "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”";
-const tanyaImg =
-  "http://127.0.0.1:5500/coding-bootcamp-testimonial-slider/images/image-tanya.jpg";
+const tanyaImg = "/images/image-tanya.jpg";
 const tanyaQuote =
   "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”";
 
@@ -11,9 +9,9 @@ const arrows = document.querySelectorAll(".controls span");
 const portrait = document.getElementById("portrait");
 const quote = document.querySelector("p");
 
-console.log(arrows);
-console.log(portrait.src);
-console.log(quote);
+// console.log(arrows);
+// console.log(portrait.src);
+// console.log(quote);
 
 arrows.forEach((arrow) => {
   arrow.addEventListener("click", () => {
