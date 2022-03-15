9221bf383ecc83d3e33e6f90de7b5eebb0cf72a3
Abdelghafour <ghafourragg@gmail.com>
2022-03-15 19:55:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// const johnImg = "./images/image-john.jpg";
-const johnImg =
-  "https://abdelghafour122.github.io/Coding-Bootcamp-Testimonial-Slider/images/image-john.jpg";
+const johnImg = "/images/image-john.jpg";
 const johnQuote =
   "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”";
-// const tanyaImg = "./images/image-tanya.jpg";
-const tanyaImg =
-  "https://abdelghafour122.github.io/Coding-Bootcamp-Testimonial-Slider/images/image-tanya.jpg";
+const tanyaImg = "/images/image-tanya.jpg";
 const tanyaQuote =
   "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”";
 
@@ -17,12 +13,17 @@ console.log(portrait.src);
 
 arrows.forEach((arrow) => {
   arrow.addEventListener("click", () => {
-    if (quote.innerText === tanyaQuote && portrait.src == tanyaImg) {
+    console.log(portrait.src.toString().includes(tanyaImg));
+    if (portrait.src.toString().includes(tanyaImg)) {
       quote.innerText = johnQuote;
-      portrait.src = johnImg;
-    } else if (quote.innerText === johnQuote && portrait.src == johnImg) {
+      // portrait.src = johnImg;
+      portrait.src =
+        "https://abdelghafour122.github.io/Coding-Bootcamp-Testimonial-Slider/images/image-john.jpg";
+    } else if (portrait.src.toString().includes(johnImg)) {
       quote.innerText = tanyaQuote;
-      portrait.src = tanyaImg;
+      // portrait.src = tanyaImg;
+      portrait.src =
+        "https://abdelghafour122.github.io/Coding-Bootcamp-Testimonial-Slider/images/image-tanya.jpg";
     }
   });
 });
